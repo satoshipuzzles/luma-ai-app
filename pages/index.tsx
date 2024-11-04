5dc1cc7340aca669dcd3a246cf2a762de6ab48c7
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2024-11-04 12:14:49 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -525,6 +525,7 @@ export default function Home() {
     <div className="min-h-screen bg-[#111111] text-white">
       <Head>
         <title>Animal Sunset 🌞🦒</title>
+        <link rel="icon" href="🌞🦒" />
          {/* Open Graph Meta Tags */}
   <meta name="description" content="Animal Sunset - AI-powered video generator." />
   <meta property="og:title" content="Animal Sunset" />
