24ee2be0f4a44f77ba6817797b3cd4b29fbf5ff9
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2024-11-08 10:29:55 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import '@shadcn/ui/styles.css';
-import { init } from '@getalby/bitcoin-connect-react';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  init({
-    appName: 'My Lightning App',
-    // add any other configuration options as needed
-  });
+
 
   return <Component {...pageProps} />;
 }
