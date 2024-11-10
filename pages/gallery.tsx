370a3b645593359b4631b4faaf8b16efcaeb05dc
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2024-11-10 15:36:34 -0800

    Update gallery.tsx

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -326,7 +326,7 @@ const handleComment = async () => {
     setProcessingAction('comment');
 
     if (selectedPost) {
-      await publishComment(selectedPost.event.id, newComment, commentParentId);
+      await publishComment(selectedPost.event.id, newComment, commentParentId || undefined);
 
       // Refresh posts to show new comment
       await fetchPosts();
