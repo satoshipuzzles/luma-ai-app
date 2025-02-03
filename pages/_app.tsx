d447f60ec4ac29554a97d919712282954876a2f2
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2025-02-02 17:16:40 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
+// pages/_app.tsx
 import type { AppProps } from 'next/app';
 import { Toaster } from "@/components/ui/toaster";
+import { FeeProvider } from '@/context/FeeContext';
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <FeeProvider>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </FeeProvider>
   );
 }
 
