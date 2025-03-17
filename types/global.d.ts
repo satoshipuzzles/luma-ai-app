56cc1740faa2afe5fbd54eb1472238eddcbec894
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2025-03-17 10:01:37 -0700

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,5 +2,18 @@ interface Window {
   nostr?: {
     getPublicKey(): Promise<string>;
     signEvent(event: any): Promise<any>;
+    getEventHash?(event: any): string;
+  }
+  bitcoinConnect?: {
+    isEnabled: boolean;
+    enable: () => Promise<void>;
+    getInfo: () => Promise<{
+      network: string;
+      address: string;
+    }>;
+    sendPayment: (invoice: string) => Promise<{
+      preimage: string;
+      paymentHash: string;
+    }>;
   }
 }
